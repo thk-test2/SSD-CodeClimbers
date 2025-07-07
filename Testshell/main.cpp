efa5bd7f243ecc67c0fec3c83eb6ad39c8868c52
Jinchan <ghkdwlscks@gmail.com>
2025-07-07 14:34:23 +0900

    [feature] Add TestShell Skeleton Code

--- Testshell/main.cpp
+++ Testshell/main.cpp
@@ -1,5 +1,38 @@
 #include "gmock/gmock.h"
 
+class SSD_INTERFACE {
+public:
+  virtual bool read(int lba);
+  virtual bool write(int lba, int value);
+};
+
+// MockDirver
+class MockSSD : public SSD_INTERFACE {
+public:
+  MOCK_METHOD(bool, read, (int lba), (override));
+  MOCK_METHOD(bool, write, (int lba, int value), (override));
+};
+
+class stdInOut_Ctrl {
+
+};
+
+class ArgParseing {
+
+};
+
+class ssd_driver : public SSD_INTERFACE {
+
+};
+
+class TestShell {
+private:
+  stdInOut_Ctrl *ctrl;
+  ArgParseing *arg;
+  ssd_driver *ssd;
+};
+
+
 int main() {
     ::testing::InitGoogleMock();
     return RUN_ALL_TESTS();
