2253335e2754fac3512d5586910d17897bf8de01
Yerim <yryun94@gmail.com>
2025-07-07 16:16:03 +0900

    [feature] add MockDriver
    Signed-off-by: Yerim <yryun94@gmail.com>

--- SSD/ssdDriver.cpp
+++ SSD/ssdDriver.cpp
@@ -1,12 +1,14 @@
-class SSDDriver {
+#include "device.h"
+
+class SSDDriver : public Device{
 public:
-  bool read(int lba) {
+  bool read(int lba) override {
     if (lba < 0 || lba > 100)
       return false;
     else
       return true;
   }
-  bool write(int lba, int value) {
+  bool write(int lba, unsigned int value) override {
     if (lba < 0 || lba > 100)
       return false;
     else
--- SSD/ssdDriver_test.cpp
+++ SSD/ssdDriver_test.cpp
@@ -1,22 +1,38 @@
 #include "gmock/gmock.h"
 #include "ssdDriver.cpp"
 
+using namespace testing;
+
+class MockSSD : public Device {
+public:
+  MOCK_METHOD(bool, write, (int, unsigned int), (override));
+  MOCK_METHOD(bool, read, (int), (override));
+};
+
 TEST(SSD_TS, ReadPass) {
-  SSDDriver ssdDriver;
+  MockSSD ssdDriver;
+  EXPECT_CALL(ssdDriver, read(AllOf(Ge(0), Lt(100))))
+	  .WillRepeatedly(Return(true));
   EXPECT_TRUE(ssdDriver.read(20));
 }
 
 TEST(SSD_TS, ReadFailOutOfRange) {
-  SSDDriver ssdDriver;
+  MockSSD ssdDriver;
+  EXPECT_CALL(ssdDriver, read(AnyOf(Lt(0), Ge(100))))
+      .WillRepeatedly(Return(false));
   EXPECT_FALSE(ssdDriver.read(200));
 }
 
 TEST(SSD_TS, WritePass) {
-  SSDDriver ssdDriver;
+  MockSSD ssdDriver;
+  EXPECT_CALL(ssdDriver, write(AllOf(Ge(0), Lt(100)),_))
+      .WillRepeatedly(Return(true));
   EXPECT_TRUE(ssdDriver.write(20,0x12345678));
 }
 
 TEST(SSD_TS, WriteFailOutOfRange) {
-  SSDDriver ssdDriver;
+  MockSSD ssdDriver;
+  EXPECT_CALL(ssdDriver, write(AnyOf(Lt(0), Ge(100)),_))
+      .WillRepeatedly(Return(false));
   EXPECT_FALSE(ssdDriver.write(200, 0x12345678));
 }
