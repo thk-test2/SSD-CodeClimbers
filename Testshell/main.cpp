a8ab700e2dd84b583e8a48477ebe942e2a72037d
Jinchan <ghkdwlscks@gmail.com>
2025-07-07 14:53:03 +0900

    [feature] skeleton code 추가

--- Testshell/main.cpp
+++ Testshell/main.cpp
@@ -1,9 +1,13 @@
 #include "gmock/gmock.h"
+#include <string>
+
+using std::string;
 
 class SSD_INTERFACE {
 public:
   virtual bool read(int lba);
   virtual bool write(int lba, int value);
+  virtual string getResult();
 };
 
 // MockDirver
@@ -11,6 +15,7 @@ class MockSSD : public SSD_INTERFACE {
 public:
   MOCK_METHOD(bool, read, (int lba), (override));
   MOCK_METHOD(bool, write, (int lba, int value), (override));
+  MOCK_METHOD(string, getResult, (), (override));
 };
 
 class StdInOutCtrl {
