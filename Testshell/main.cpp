f17d917302bfc8a80c7b62c400b7715c956c0689
Sunghwan Kim <sung94.kim@samsung.com>
2025-07-07 15:51:27 +0900

    [feature] Add TestShell class common method and variable

--- Testshell/main.cpp
+++ Testshell/main.cpp
@@ -1,16 +1,21 @@
 #include "gmock/gmock.h"
 #include <string>
+#include <iostream>
+#include <vector>
 
 using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
 
 class SSD_INTERFACE {
 public:
-  virtual bool read(int lba);
-  virtual bool write(int lba, int value);
-  virtual string getResult();
+  virtual bool read(int lba) = 0;
+  virtual bool write(int lba, int value) = 0;
+  virtual string getResult() = 0;
 };
 
-// MockDirver
+// MockDriver
 class MockSSD : public SSD_INTERFACE {
 public:
   MOCK_METHOD(bool, read, (int lba), (override));
@@ -18,7 +23,14 @@ public:
   MOCK_METHOD(string, getResult, (), (override));
 };
 
+class Command {
+public:
+  string command;
+  vector<string> args;
+};
+
 class StdInOutCtrl {
+public:
 
 };
 
@@ -27,7 +39,10 @@ class ArgParser {
 };
 
 class SsdInterface : public SSD_INTERFACE {
-
+public:
+  bool read(int lba) override { return true; }
+  bool write(int lba, int value) override { return true; }
+  string getResult() override { return ""; }
 };
 
 class TestShell {
@@ -35,10 +50,60 @@ private:
   StdInOutCtrl *ctrl;
   ArgParser *parser;
   SsdInterface *ssd;
+  Command command;
+
+public:
+  TestShell() : ctrl(new StdInOutCtrl()), parser(new ArgParser()), ssd(new SsdInterface()) {}
+
+  void run() {
+    string userInput;
+    cout << "SSD Test Shell - Type 'help' for userInputs" << endl;
+    while (true) {
+      cout << "> ";
+      getline(std::cin, userInput);
+      command = parsing(userInput);
+      if (command.command == "exit") {
+        break;
+      }
+      executeCommand(command);
+    }
+  }
+
+  void executeCommand(const Command& command) {
+    if (command.command == "read") {
+
+    } else if (command.command == "write") {
+
+    } else if (command.command == "fullread") {
+
+    } else if (command.command == "fullwrite") {
+
+    } else if (command.command == "help") {
+
+    } else if (command.command == "1_") {
+
+    } else if (command.command == "2_") {
+
+    } else if (command.command == "3_") {
+
+    } else {
+      cout << "INVALID COMMAND" << endl;
+    }
+  }
+
+  Command parsing(const string& userInput) {
+    return Command{userInput,
+                   vector<string>()}; // Simplified parsing for demonstration
+  }
 };
 
-
 int main() {
-    ::testing::InitGoogleMock();
-    return RUN_ALL_TESTS();
+#ifdef _DEBUG
+  ::testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
+#else
+  TestShell testShell;
+  testShell.run();
+  return 0;
+#endif
 }
