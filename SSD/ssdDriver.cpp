e4e05eb0de1666ee10004a9a9471aa3d6efaf009
Hyunseok <lukiaeve@gmail.com>
2025-07-08 08:19:44 +0900

    [fix] resolve conflict & add prev changes

--- SSD/ssdDriver.cpp
+++ SSD/ssdDriver.cpp
@@ -1,7 +1,39 @@
 #include "device.h"
+#include <vector>
+#include <string>
+#include <iostream>
+
+using std::vector;
+using std::string;
 
 class SSDDriver : public Device{
 public:
+    void run(int argc, char* argv[]) {
+        if (argc < 3) std::exception();
+        if (argv[0] != "ssd.exe") std::exception();
+
+        vector<string> params = parseParams(argc, argv);
+
+        string command = params[0];
+        if (command == "W") {
+            //mock
+        }
+        else if (command == "R") {
+
+        }
+        else {
+            std::exception();
+        }
+    }
+
+    static vector<string> parseParams(int argc, char* argv[]) {
+        vector<string> params;
+        for (int i = 1; i < argc; ++i) {
+            params.push_back(argv[i]);
+        }
+        return params;
+    }
+
   bool read(int lba) override {
     if (lba < 0 || lba > 100)
       return false;
