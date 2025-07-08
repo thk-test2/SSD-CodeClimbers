62cafac7893f53d80e8acd3c2a26a26fe1479496
Hyunseok <lukiaeve@gmail.com>
2025-07-08 10:12:20 +0900

    [feature] add run func

--- SSD/ssdDriver.cpp
+++ SSD/ssdDriver.cpp
@@ -9,7 +9,22 @@ using std::string;
 class SSDDriver : public Device{
 public:
     void run(int argc, char* argv[]) {
+        if (argc < 3) std::exception();
+        if (argv[0] != "ssd.exe") std::exception();
 
+        vector<string> params = parseParams(argc, argv);
+
+        string command = params[1];
+        int lba = std::stoi(params[2]);
+        int value = std::stoul(params[3]);
+
+        if (command == "W") {
+        }
+        else if (command == "R") {
+        }
+        else {
+            std::exception();
+        }
     }
 
     vector<string> parseParams(int argc, char* argv[]) {
