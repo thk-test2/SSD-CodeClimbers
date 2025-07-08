4454ef147daac077dba5d1630c3a591f287182e2
Hyunseok <lukiaeve@gmail.com>
2025-07-08 10:28:38 +0900

    [fix] apply LLVM coding stye#2

--- SSD/ssdDriver.cpp
+++ SSD/ssdDriver.cpp
@@ -1,50 +1,50 @@
 #include "device.h"
-#include <vector>
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
 
-using std::vector;
 using std::string;
+using std::vector;
 
 class SSDDriver : public Device {
 public:
-    void run(int argc, char* argv[]) {
-        if (argc < 3) std::exception();
-        if (argv[0] != "ssd.exe") std::exception();
+  void run(int argc, char *argv[]) {
+    if (argc < 3)
+      std::exception();
+    if (argv[0] != "ssd.exe")
+      std::exception();
 
-        vector<string> params = parseParams(argc, argv);
+    vector<string> params = parseParams(argc, argv);
 
-        string command = params[1];
-        int lba = std::stoi(params[2]);
-        int value = std::stoul(params[3]);
+    string command = params[1];
+    int lba = std::stoi(params[2]);
+    int value = std::stoul(params[3]);
 
-        if (command == "W") {
-        }
-        else if (command == "R") {
-        }
-        else {
-            std::exception();
-        }
+    if (command == "W") {
+    } else if (command == "R") {
+    } else {
+      std::exception();
     }
+  }
 
-    vector<string> parseParams(int argc, char* argv[]) {
-        vector<string> params;
-        for (int i = 1; i < argc; ++i) {
-            params.push_back(argv[i]);
-        }
-        return params;
+  vector<string> parseParams(int argc, char *argv[]) {
+    vector<string> params;
+    for (int i = 1; i < argc; ++i) {
+      params.push_back(argv[i]);
     }
+    return params;
+  }
 
-    bool read(int lba) override {
-        if (lba < 0 || lba > 100)
-            return false;
-        else
-            return true;
-    }
-    bool write(int lba, unsigned int value) override {
-        if (lba < 0 || lba > 100)
-            return false;
-        else
-            return true;
-    }
+  bool read(int lba) override {
+    if (lba < 0 || lba > 100)
+      return false;
+    else
+      return true;
+  }
+  bool write(int lba, unsigned int value) override {
+    if (lba < 0 || lba > 100)
+      return false;
+    else
+      return true;
+  }
 };
--- SSD/ssdDriver_test.cpp
+++ SSD/ssdDriver_test.cpp
@@ -1,92 +1,89 @@
-#include "gmock/gmock.h"
 #include "ssdDriver.cpp"
+#include "gmock/gmock.h"
 
 using namespace testing;
 
 class MockSSD : public Device {
 public:
-    MOCK_METHOD(bool, write, (int, unsigned int), (override));
-    MOCK_METHOD(bool, read, (int), (override));
-    MOCK_METHOD(void, run, (int, char* []), (override));
+  MOCK_METHOD(bool, write, (int, unsigned int), (override));
+  MOCK_METHOD(bool, read, (int), (override));
+  MOCK_METHOD(void, run, (int, char *[]), (override));
 };
 
 TEST(SSD_TS, ReadPass) {
-    MockSSD ssdDriver;
-    EXPECT_CALL(ssdDriver, read(AllOf(Ge(0), Lt(100))))
-        .WillRepeatedly(Return(true));
-    EXPECT_TRUE(ssdDriver.read(20));
+  MockSSD ssdDriver;
+  EXPECT_CALL(ssdDriver, read(AllOf(Ge(0), Lt(100))))
+      .WillRepeatedly(Return(true));
+  EXPECT_TRUE(ssdDriver.read(20));
 }
 
 TEST(SSD_TS, ReadFailOutOfRange) {
-    MockSSD ssdDriver;
-    EXPECT_CALL(ssdDriver, read(AnyOf(Lt(0), Ge(100))))
-        .WillRepeatedly(Return(false));
-    EXPECT_FALSE(ssdDriver.read(200));
+  MockSSD ssdDriver;
+  EXPECT_CALL(ssdDriver, read(AnyOf(Lt(0), Ge(100))))
+      .WillRepeatedly(Return(false));
+  EXPECT_FALSE(ssdDriver.read(200));
 }
 
 TEST(SSD_TS, WritePass) {
-    MockSSD ssdDriver;
-    EXPECT_CALL(ssdDriver, write(AllOf(Ge(0), Lt(100)), _))
-        .WillRepeatedly(Return(true));
-    EXPECT_TRUE(ssdDriver.write(20, 0x12345678));
+  MockSSD ssdDriver;
+  EXPECT_CALL(ssdDriver, write(AllOf(Ge(0), Lt(100)), _))
+      .WillRepeatedly(Return(true));
+  EXPECT_TRUE(ssdDriver.write(20, 0x12345678));
 }
 
 TEST(SSD_TS, WriteFailOutOfRange) {
-    MockSSD ssdDriver;
-    EXPECT_CALL(ssdDriver, write(AnyOf(Lt(0), Ge(100)), _))
-        .WillRepeatedly(Return(false));
-    EXPECT_FALSE(ssdDriver.write(200, 0x12345678));
+  MockSSD ssdDriver;
+  EXPECT_CALL(ssdDriver, write(AnyOf(Lt(0), Ge(100)), _))
+      .WillRepeatedly(Return(false));
+  EXPECT_FALSE(ssdDriver.write(200, 0x12345678));
 }
 
 TEST(SSD_TS, Run) {
-    MockSSD ssdDriver;
-    int argc = 3;
-    char* argv[3];
-    argv[0] = const_cast<char*>("ssd.exe");
-    argv[1] = const_cast<char*>("R");
-    argv[2] = const_cast<char*>("10");
+  MockSSD ssdDriver;
+  int argc = 3;
+  char *argv[3];
+  argv[0] = const_cast<char *>("ssd.exe");
+  argv[1] = const_cast<char *>("R");
+  argv[2] = const_cast<char *>("10");
 
-    EXPECT_CALL(ssdDriver, run(argc, argv))
-        .Times(1);
-    ssdDriver.run(argc, argv);
+  EXPECT_CALL(ssdDriver, run(argc, argv)).Times(1);
+  ssdDriver.run(argc, argv);
 }
 
 TEST(SSD_TS, RunInvalidParam1) {
-    SSDDriver ssdDriver;
-    //ssd.exe R 10 0x00010001 
-    int argc = 1;
-    char* argv[1];
-    argv[0] = const_cast<char*>("ssd.exe");
+  SSDDriver ssdDriver;
+  // ssd.exe R 10 0x00010001
+  int argc = 1;
+  char *argv[1];
+  argv[0] = const_cast<char *>("ssd.exe");
 
-    vector<string> params = ssdDriver.parseParams(argc, argv);
+  vector<string> params = ssdDriver.parseParams(argc, argv);
 
-    EXPECT_EQ(params.size(), 0);
+  EXPECT_EQ(params.size(), 0);
 }
 
 TEST(SSD_TS, RunInvalidParam2) {
-    SSDDriver ssdDriver;
-    int argc = 3;
-    char* argv[3];
-    argv[0] = const_cast<char*>("ssd.exe");
-    argv[1] = const_cast<char*>("R");
-    argv[2] = const_cast<char*>("10");
-    vector<string> params = ssdDriver.parseParams(argc, argv);
+  SSDDriver ssdDriver;
+  int argc = 3;
+  char *argv[3];
+  argv[0] = const_cast<char *>("ssd.exe");
+  argv[1] = const_cast<char *>("R");
+  argv[2] = const_cast<char *>("10");
+  vector<string> params = ssdDriver.parseParams(argc, argv);
 
-    EXPECT_EQ(params.size(), 2);
+  EXPECT_EQ(params.size(), 2);
 }
 
-TEST(SSD_TS, WriteParams)
-{
-    SSDDriver ssdDriver;
-    int argc = 4;
-    char* argv[4];
-    argv[0] = const_cast<char*>("ssd.exe");
-    argv[1] = const_cast<char*>("W");
-    argv[2] = const_cast<char*>("10");
-    argv[3] = const_cast<char*>("0x12345678");
+TEST(SSD_TS, WriteParams) {
+  SSDDriver ssdDriver;
+  int argc = 4;
+  char *argv[4];
+  argv[0] = const_cast<char *>("ssd.exe");
+  argv[1] = const_cast<char *>("W");
+  argv[2] = const_cast<char *>("10");
+  argv[3] = const_cast<char *>("0x12345678");
 
-    vector<string> args = ssdDriver.parseParams(argc, argv);
+  vector<string> args = ssdDriver.parseParams(argc, argv);
 
-    EXPECT_EQ(args.size(), 3);
+  EXPECT_EQ(args.size(), 3);
 }
-
