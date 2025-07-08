816ae5c625a648a0f0e0707bce3b1853553a79b0
Sunghwan Kim <sung94.kim@samsung.com>
2025-07-08 09:12:02 +0900

    [feature] add help() function

--- Testshell/test_shell.cpp
+++ Testshell/test_shell.cpp
@@ -79,7 +79,7 @@ public:
     } else if (command.command == "fullwrite") {
 
     } else if (command.command == "help") {
-
+      help();
     } else if (command.command == "1_") {
 
     } else if (command.command == "2_") {
@@ -94,5 +94,43 @@ public:
   Command parsing(const string &userInput) {
     return Command{userInput,
                    vector<string>()}; // Simplified parsing for demonstration
+  }
+  void help() {
+    cout << "\033[1;36m"
+         << "SSD Test Shell - Simple and Powerful SSD Testing Tool\n"
+         << "\033[0m";
+
+    cout << "\n\033[1mTeam: CodeClimbers\n\033[0m";
+
+    cout << "\n\033[1mTeam Members:\033[0m\n"
+         << "  Taehyeon Kyung, Sunghwan Kim, Hyeonseok Sim\n"
+         << "  Yerim Yun, Hoenhwi Jeong, Jeseong Kim\n"
+         << "  Repository: https://github.com/thk-test2/SSD-CodeClimbers\n";
+
+    cout << "\n\033[1mCommands:\033[0m\n"
+         << "  \033[1mread\033[0m <lba>\n"
+         << "    Description: Read from SSD at logical block address <lba>\n"
+         << "    Example: read 10\n\n"
+
+         << "  \033[1mwrite\033[0m <lba> <value>\n"
+         << "    Description: Write <value> to SSD at logical block address "
+            "<lba>\n"
+         << "    Example: write 5 0xFF\n\n"
+
+         << "  \033[1mfullread\033[0m\n"
+         << "    Description: Read the entire SSD\n"
+         << "    Example: fullread\n\n"
+
+         << "  \033[1mfullwrite\033[0m <value>\n"
+         << "    Description: Write <value> to the entire SSD\n"
+         << "    Example: fullwrite 0x00\n\n"
+
+         << "  \033[1mhelp\033[0m\n"
+         << "    Description: Show this help message\n"
+         << "    Example: help\n\n"
+
+         << "  \033[1mexit\033[0m\n"
+         << "    Description: Exit the shell\n"
+         << "    Example: exit\n";
   }
 };
