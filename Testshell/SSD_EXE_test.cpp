7177e4079280721d620146eb23c655b957927a82
jeseong.kim <dkgkrnrn@gmail.com>
2025-07-08 14:50:18 +0900

    [refactoring] SSD_EXE

--- Testshell/SSD_EXE_test.cpp
+++ Testshell/SSD_EXE_test.cpp
@@ -1,31 +1,25 @@
 #include "SSD_EXE.cpp"
 #include "gmock/gmock.h"
+using testing::internal::CaptureStdout;
+using testing::internal::GetCapturedStdout;
 
-//class MockSSD_EXE : public SSD_EXE {
-//public:
-//  MOCK_METHOD(void, read, (int lba), ());
-//  MOCK_METHOD(void, write, (int lba, unsigned long value), ());
-//  MOCK_METHOD(std::string, getResult, (), ());
-//};
-
-TEST(SSDEXE, readNormal) {
-  using testing::internal::CaptureStdout;
-  using testing::internal::GetCapturedStdout;
+class SSDEXEFixture : public ::testing::Test {
+public:
   SSD_EXE ssd;
+};
 
+TEST_F(SSDEXEFixture, readNormal) {
   CaptureStdout();
   ssd.read(0);
   std::string output = GetCapturedStdout();
-  EXPECT_TRUE(output.find("ssd.exe\" R 0") != std::string::npos);
+  EXPECT_TRUE(output.find("ssd.exe\" R 0") != std::string::npos)
+	  << "Actual stream : " << output;
 }
 
-TEST(SSDEXE, writeNormal) {
-  using testing::internal::CaptureStdout;
-  using testing::internal::GetCapturedStdout;
-  SSD_EXE ssd;
-
+TEST_F(SSDEXEFixture, writeNormal) {
   CaptureStdout();
   ssd.write(0, 0xAAAABBBB);
   std::string output = GetCapturedStdout();
-  EXPECT_TRUE(output.find("ssd.exe\" W 0xAAAABBBB") != std::string::npos);
+  EXPECT_TRUE(output.find("ssd.exe\" W 0 0xAAAABBBB") != std::string::npos)
+      << "Actual stream : " << output;
 }
