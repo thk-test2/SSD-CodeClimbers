f0e9b71e376530841a9b47195249e9088127b4d1
hh92.jeong <jhh1472@nate.com>
2025-07-08 17:46:35 +0900

    feature/SsdUnitTest

--- SSD/ssd_driver_test.cpp
+++ SSD/ssd_driver_test.cpp
@@ -85,9 +85,43 @@ TEST(SSD_TS, WriteParams) {
 
   EXPECT_EQ(args.size(), 3);
 }
+class SSDDriverTestData {
+public:
+  int lba;
+  unsigned long value;
+
+  SSDDriverTestData(int lba_, unsigned long value_)
+      : lba(lba_), value(value_) {}
+};
+
 class SSDDriverTestFixture : public Test {
 public:
   SSDDriver ssd;
+  const int MAX_NAND_MEMORY_MAP_SIZE = 100;
+
+  void checkOutputFileValid(const string &expected_value) {
+    EXPECT_EQ(expected_value, ssd.getIoStream()->readFileAsString(
+                                  ssd.getIoStream()->output_file_name));
+  }
+
+  void checkSpecificLineInNandFile(const string &expected_value,
+                                   int targetLine) {
+    EXPECT_GE(targetLine, 0);
+    EXPECT_LT(targetLine, MAX_NAND_MEMORY_MAP_SIZE);
+
+    std::istringstream iss(
+        ssd.getIoStream()->readFileAsString(ssd.getIoStream()->nand_file_name));
+    string getLine = "";
+    string compareLine = std::to_string(targetLine) + " " + expected_value;
+
+    for (int i = 0; i < MAX_NAND_MEMORY_MAP_SIZE; i++) {
+      std::getline(iss, getLine);
+      if (i == targetLine) {
+        EXPECT_EQ(getLine, compareLine);
+        break;
+      }
+    }
+  }
 };
 
 TEST_F(SSDDriverTestFixture, SSDReadInitialPass) {
@@ -95,10 +129,7 @@ TEST_F(SSDDriverTestFixture, SSDReadInitialPass) {
   int lba = 2;
   ssd.read(lba);
 
-  istringstream iss = ssd.getIoStream()->getOutputReadStream();
-  getline(iss, output);
-
-  EXPECT_EQ("0x00000000", output);
+  checkOutputFileValid("0x00000000");
 }
 
 TEST_F(SSDDriverTestFixture, SSDReadOutOfRangeFail) {
@@ -106,10 +137,7 @@ TEST_F(SSDDriverTestFixture, SSDReadOutOfRangeFail) {
   int lba = 100;
   ssd.read(lba);
 
-  istringstream iss = ssd.getIoStream()->getOutputReadStream();
-  getline(iss, output);
-
-  EXPECT_EQ("ERROR", output);
+  checkOutputFileValid("ERROR");
 }
 
 TEST_F(SSDDriverTestFixture, SSDWritePass) {
@@ -117,10 +145,20 @@ TEST_F(SSDDriverTestFixture, SSDWritePass) {
   int lba = 1;
   ssd.write(lba, 0xAAAAFFFF);
 
-  istringstream iss = ssd.getIoStream()->getOutputReadStream();
-  getline(iss, output);
+  checkOutputFileValid("");
+}
 
-  EXPECT_EQ("", output);
+TEST_F(SSDDriverTestFixture, SSDReadAfterWritePass) {
+  string output;
+  int lba = 1;
+  long value = 0;
+  ssd.write(lba, 0xAAAAFFFF);
+
+  checkOutputFileValid("");
+
+  EXPECT_TRUE(ssd.read(lba));
+  checkOutputFileValid("0xAAAAFFFF");
+  checkSpecificLineInNandFile("0xAAAAFFFF", lba);
 }
 
 TEST_F(SSDDriverTestFixture, SSDWriteOutOfRange) {
@@ -128,10 +166,7 @@ TEST_F(SSDDriverTestFixture, SSDWriteOutOfRange) {
   int lba = 100;
   ssd.write(lba, 0xAAAAFFFF);
 
-  istringstream iss = ssd.getIoStream()->getOutputReadStream();
-  getline(iss, output);
-
-  EXPECT_EQ("ERROR", output);
+  checkOutputFileValid("ERROR");
 }
 
 TEST_F(SSDDriverTestFixture, SSDReadWriteCompare) {
@@ -140,10 +175,7 @@ TEST_F(SSDDriverTestFixture, SSDReadWriteCompare) {
   ssd.write(1, 0xAAAAFFFF);
   ssd.read(1);
 
-  istringstream iss = ssd.getIoStream()->getOutputReadStream();
-  getline(iss, output);
-
-  EXPECT_EQ("0xAAAAFFFF", output);
+  checkOutputFileValid("0xAAAAFFFF");
 }
 
 TEST_F(SSDDriverTestFixture, SSDWrite2TimesCompare) {
@@ -153,8 +185,41 @@ TEST_F(SSDDriverTestFixture, SSDWrite2TimesCompare) {
   ssd.write(1, 0xEEEEAAAA);
   ssd.read(1);
 
-  istringstream iss = ssd.getIoStream()->getOutputReadStream();
-  getline(iss, output);
+  checkOutputFileValid("0xEEEEAAAA");
+}
 
-  EXPECT_EQ("0xEEEEAAAA", output);
+TEST_F(SSDDriverTestFixture, SSDCheckSsdNandFileValidAfterWrite) {
+  std::vector<SSDDriverTestData> writeDataVector;
+  std::istringstream backup_iss(
+      ssd.getIoStream()->readFileAsString(ssd.getIoStream()->nand_file_name));
+  string beforeTxtLine = "", AfterTxtLine = "";
+  int lineNo = 0;
+  int mismatchLineNum = 0;
+
+  // ssd_nand.txt write
+  writeDataVector.push_back(SSDDriverTestData(1, 0x00000001));
+  writeDataVector.push_back(SSDDriverTestData(2, 0x00000002));
+  writeDataVector.push_back(SSDDriverTestData(3, 0x00000003));
+  writeDataVector.push_back(SSDDriverTestData(10, 0x00000010));
+  writeDataVector.push_back(SSDDriverTestData(99, 0x00000099));
+
+  for (const auto &data : writeDataVector) {
+    ssd.write(data.lba, data.value);
+  }
+
+  // Read again nana_file.txt
+  std::istringstream iss(
+      ssd.getIoStream()->readFileAsString(ssd.getIoStream()->nand_file_name));
+
+  while (std::getline(backup_iss, beforeTxtLine) &&
+         std::getline(iss, AfterTxtLine)) {
+
+    if (beforeTxtLine != AfterTxtLine) {
+      EXPECT_EQ(lineNo, writeDataVector[mismatchLineNum].lba);
+      EXPECT_EQ(stoul(AfterTxtLine, nullptr, 16),
+                writeDataVector[mismatchLineNum].value);
+      mismatchLineNum++;
+    }
+    lineNo++;
+  }
 }
