a6b7284a8f2b7e10f45263c420a6fc728616ccc1
hyunseok <lukiaeve@gmail.com>
2025-07-09 14:34:31 +0900

    [ut] add test cases

--- SSD/ssd_cmd_buffer_flush_test.cpp
+++ SSD/ssd_cmd_buffer_flush_test.cpp
@@ -1,13 +1,39 @@
 #include "gmock/gmock.h"
 #include "ssd_cmd_buffer_control.h"
-
+#include "ssd_driver.cpp"
 using std::cout;
-
 using namespace testing;
 
 class BufctrlFlushFixture : public Test {
 public:
   CmdBufferControl &cmdBuffer = CmdBufferControl::getInstance();
+  SSDDriver driver;
+  std::vector<Cmd> result;
 
-  const std::string BUFFER_EMPTY = "1_empty,2_empty,3_empty,4_empty,5_empty,";
+  void parse(const std::string &input) {
+    result = cmdBuffer.parseCommands(input);
+  }
 };
+
+TEST_F(BufctrlFlushFixture, ParsesSize) {
+  parse("1_empty,2_W_42_0xDEADBEEF,3_empty,");
+  ASSERT_EQ(result.size(), 5);
+}
+
+TEST_F(BufctrlFlushFixture, ParsesWrite) {
+  parse("1_empty,2_W_42_0xDEADBEEF,3_empty,");
+  EXPECT_EQ(result[0].cmdType, std::string("empty"));
+}
+
+TEST_F(BufctrlFlushFixture, ThrowsInvalid) {
+  EXPECT_THROW(parse("1_W_42,"), std::invalid_argument); 
+}
+
+TEST_F(BufctrlFlushFixture, IgnoresUnknownCmdType) {
+  parse("1_UNKNOWN_123_ABC,");
+  EXPECT_TRUE(result.empty());
+}
+
+TEST_F(BufctrlFlushFixture, IgnoresUnknownCmdType) {
+  EXPECT_NO_THROW(cmdBuffer.flush());
+}
