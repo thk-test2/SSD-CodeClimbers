e9315427843ce967a06816bbcd17eac1866a35b6
Sunghwan Kim <sung94.kim@samsung.com>
2025-07-09 17:07:27 +0900

    [refactoring] Fix fullread/fullwrite logic

--- Testshell/fullread_command.cpp
+++ Testshell/fullread_command.cpp
@@ -7,14 +7,13 @@ bool FullReadCommand::execute(TestShell* shell, const Command& command) {
     return false;
   }
   
-  int lba = 0;
-  shell->getSSD()->read(lba);
-  string result = shell->getSSD()->getResult();
-  while (result != "ERROR") {
-    cout << "[Full Read] LBA: " << lba << " Result: " << result << endl;
-    lba++;
+  for (int lba = 0; lba < MAX_LBA_COUNT; lba++) {
     shell->getSSD()->read(lba);
-    result = shell->getSSD()->getResult();
+    string result = shell->getSSD()->getResult();
+    if (result == "ERROR") {
+      break;
+    }
+    cout << "[Full Read] LBA: " << lba << " Result: " << result << endl;
   }
   return true;
 }
--- Testshell/fullwrite_command.cpp
+++ Testshell/fullwrite_command.cpp
@@ -1,12 +1,12 @@
 #include "command.h"
 #include "test_shell.h"
 
-bool FullWriteCommand::execute(TestShell* shell, const Command& command) {
+bool FullWriteCommand::execute(TestShell *shell, const Command &command) {
   if (command.args.size() != 1) {
     cout << "INVALID COMMAND\n";
     return false;
   }
-  
+
   unsigned long value;
   try {
     value = stoul(command.args[0], nullptr, shell->getHexBase());
@@ -14,15 +14,14 @@ bool FullWriteCommand::execute(TestShell* shell, const Command& command) {
     cout << "INVALID COMMAND\n";
     return false;
   }
-  
-  int lba = 0;
-  shell->getSSD()->write(lba, value);
-  string result = shell->getSSD()->getResult();
-  while (result != "ERROR") {
-    cout << "[Full Write] LBA: " << lba << " Done" << endl;
-    lba++;
+
+  for (int lba = 0; lba < MAX_LBA_COUNT; lba++) {
     shell->getSSD()->write(lba, value);
-    result = shell->getSSD()->getResult();
+    string result = shell->getSSD()->getResult();
+    if (result == "ERROR") {
+      break;
+    }
+    cout << "[Full Write] LBA: " << lba << " Done" << endl;
   }
   return true;
 }
