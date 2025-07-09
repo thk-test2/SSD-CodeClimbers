f4428383013e7aba16e458455539954a5ba504bc
Sunghwan Kim <sung94.kim@samsung.com>
2025-07-09 15:06:46 +0900

    [feature] Add Test Shell flush command

--- Testshell/ssd_interface.h
+++ Testshell/ssd_interface.h
@@ -10,5 +10,6 @@ public:
   virtual void read(int lba) = 0;
   virtual void write(int lba, unsigned long value) = 0;
   virtual void erase(int lba, int size) = 0;
+  virtual void flush() = 0;
   virtual string getResult() = 0;
 };
