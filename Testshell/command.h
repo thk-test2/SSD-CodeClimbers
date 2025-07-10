0b1d73b781c07845da63013f4fdd2ef402ae715e
Bejjang2 <91356113+jeseongkim@users.noreply.github.com>
2025-07-10 13:19:20 +0900

    Revert "[refactor] Refactor test script functions (#62)"
    This reverts commit 58e09eef8a02c04184182ca3c01f24775cb80a31.

--- Testshell/command.h
+++ Testshell/command.h
@@ -22,9 +22,7 @@ class FullWriteCommand : public ICommandHandler {
 public:
   bool execute(TestShell *shell, const Command &command) override;
   string getUsage() const override { return "<value>"; }
-  string getDescription() const override {
-    return "Write <value> to the entire SSD";
-  }
+  string getDescription() const override { return "Write <value> to the entire SSD"; }
   string getExample() const override { return "fullwrite 0x00"; }
 };
 
@@ -43,9 +41,7 @@ private:
 public:
   bool execute(TestShell *shell, const Command &command) override;
   string getUsage() const override { return "<lba>"; }
-  string getDescription() const override {
-    return "Read from SSD at logical block address <lba>";
-  }
+  string getDescription() const override { return "Read from SSD at logical block address <lba>"; }
   string getExample() const override { return "read 10"; }
 };
 
@@ -56,9 +52,7 @@ private:
 public:
   bool execute(TestShell *shell, const Command &command) override;
   string getUsage() const override { return "<lba> <value>"; }
-  string getDescription() const override {
-    return "Write <value> to SSD at logical block address <lba>";
-  }
+  string getDescription() const override { return "Write <value> to SSD at logical block address <lba>"; }
   string getExample() const override { return "write 5 0xFF"; }
 };
 
@@ -104,41 +98,24 @@ class TestScript1 : public ICommandHandler {
 public:
   bool execute(TestShell *shell, const Command &command) override;
   string getUsage() const override { return "<value>"; }
-  string getDescription() const override {
-    return "Run comprehensive write/read test for entire SSD";
-  }
-  string getExample() const override {
-    return "'1_' or '1_FullWriteAndReadCompare'";
-  }
-
-private:
-  bool onExecute(TestShell *shell, unsigned long value, std::string &valueStr);
+  string getDescription() const override { return "Run comprehensive write/read test for entire SSD"; }
+  string getExample() const override { return "'1_' or '1_FullWriteAndReadCompare'"; }
 };
 
 class TestScript2 : public ICommandHandler {
 public:
   bool execute(TestShell *shell, const Command &command) override;
   string getUsage() const override { return "<value>"; }
-  string getDescription() const override {
-    return "Run partial LBA write consistency test (30 iterations)";
-  }
+  string getDescription() const override { return "Run partial LBA write consistency test (30 iterations)"; }
   string getExample() const override { return "'2_' or '2_PartialLBAWrite'"; }
-
-private:
-  bool onExecute(TestShell *shell);
 };
 
 class TestScript3 : public ICommandHandler {
 public:
   bool execute(TestShell *shell, const Command &command) override;
   string getUsage() const override { return ""; }
-  string getDescription() const override {
-    return "Run write/read aging test (200 iterations)";
-  }
+  string getDescription() const override { return "Run write/read aging test (200 iterations)"; }
   string getExample() const override { return "'3_' or '3_WriteReadAging'"; }
-
-private:
-  bool onExecute(TestShell *shell, unsigned long value);
 };
 
 class TestScript4 : public ICommandHandler {
@@ -146,13 +123,7 @@ public:
   bool execute(TestShell *shell, const Command &command) override;
   string getUsage() const override { return ""; }
   string getDescription() const override {
-    return "Repeatedly erase LBA ranges and write random data to following "
-           "LBAs in sequential loops (30 iterations)";
+    return "Repeatedly erase LBA ranges and write random data to following LBAs in sequential loops (30 iterations)";
   }
-  string getExample() const override {
-    return "'4_' or '4_EraseAndWriteAging'";
-  }
-
-private:
-  bool onExecute(TestShell *shell);
+  string getExample() const override { return "'4_' or '4_EraseAndWriteAging'"; }
 };
